63c29d8fce071c730f42ce2a372aa02d0df13056
Nick Lima <nick.lima.wp@gmail.com>
2021-10-27 14:40:46 -0300

    💄 Adding Styled Components

---
+++ styles/shared.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Main = styled.div`
+  align-items: center;
+  display: flex;
+  font-family: var(--font-default);
+  flex-direction: column;
+  justify-content: center;
+  min-height: 80vh;
+  position: relative;
+  text-align: center;
+`;
